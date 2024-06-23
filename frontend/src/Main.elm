9b59e638fea21889e592272de6bdee5967d549c1
Jonas Jensen <jbj@knef.dk>
2024-06-23 21:13:52 +0200

    Reformat Main.elm with elm-format
    We have format-on-save in the settings of the workspace file, and this
    now works for Elm too. It seems like adding elm-format to the Dockerfile
    broke VSCode Elm integration completely, and I had to delete
    `frontend/elm-stuff/` to fix it. Now both formatting and language server
    works.

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,18 +1,21 @@
 module Main exposing (main)
 
-import Browser
-import Http
-import Html exposing (Html, div, text)
 import Bootstrap.Button as BButton
+import Browser
+import Html exposing (Html, div, text)
+import Http
+
 
 type Model
     = Initial
-    | Error (String)
-    | Loaded (String)
+    | Error String
+    | Loaded String
+
 
 type Msg
     = Response (Result Http.Error String)
     | ButtonClicked
+
 
 postRequest : String -> Cmd Msg
 postRequest url =
@@ -22,12 +25,22 @@ postRequest url =
         , expect = Http.expectString Response
         }
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = case msg of
-    ButtonClicked -> ( model, Cmd.batch [
-        postRequest "http://localhost:3000/hello" ])
-    Response (Ok res) -> ( Loaded res, Cmd.none )
-    Response (Err _) -> ( Error "POST request failed", Cmd.none )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ButtonClicked ->
+            ( model
+            , Cmd.batch
+                [ postRequest "http://localhost:3000/hello"
+                ]
+            )
+
+        Response (Ok res) ->
+            ( Loaded res, Cmd.none )
+
+        Response (Err _) ->
+            ( Error "POST request failed", Cmd.none )
 
 
 main : Program () Model Msg
@@ -39,9 +52,15 @@ main =
         , subscriptions = \_ -> Sub.none
         }
 
+
 view : Model -> Html Msg
 view m =
     case m of
-        Initial -> div [] [ BButton.button [ BButton.onClick (ButtonClicked), BButton.primary ] [ text "Click me" ] ]
-        Error err -> div [] [ text ("Error: " ++ err) ]
-        Loaded resp -> div [] [ text ("Response: " ++ resp) ]
+        Initial ->
+            div [] [ BButton.button [ BButton.onClick ButtonClicked, BButton.primary ] [ text "Click me" ] ]
+
+        Error err ->
+            div [] [ text ("Error: " ++ err) ]
+
+        Loaded resp ->
+            div [] [ text ("Response: " ++ resp) ]
