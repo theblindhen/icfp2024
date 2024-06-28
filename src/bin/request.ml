1a7f7ce3dcb6775d5c3a3c591a5beb09f39b681e
Jonas Jensen <jbj@knef.dk>
2024-06-28 16:05:33 +0200

    Take request from command line

--- src/bin/request.ml
+++ src/bin/request.ml
@@ -5,20 +5,23 @@ open Core
 
 (* open Lwt.Syntax (* This requires some Dune support *) *)
 open Lwt
-open Cohttp
-open Cohttp_lwt_unix
 open Lib
 
-let get_body auth_token uri =
-  let headers = Header.of_list [ ("Authorization", "Bearer " ^ auth_token) ] in
-  let body = Cohttp_lwt.Body.of_string "S'%4}).$%8" in
-  Client.post ~headers ~body (Uri.of_string uri) >>= fun (_resp, body) ->
+let get_body auth_token input =
+  let headers = Cohttp.Header.of_list [ ("Authorization", "Bearer " ^ auth_token) ] in
+  let body = Cohttp_lwt.Body.of_string (Language.encode_string input) in
+  let uri = "https://boundvariable.space/communicate" in
+  Cohttp_lwt_unix.Client.post ~headers ~body (Uri.of_string uri) >>= fun (_resp, body) ->
   Cohttp_lwt.Body.to_string body >>= fun body_string ->
   Lwt.return (Language.decode_token body_string)
 
 let () =
+  let input =
+    match Sys.get_argv () with
+    | [| _; input |] -> input
+    | _ -> failwith "Usage: request ASCII_STRING"
+  in
   (* Get auth token from env var AUTH_TOKEN *)
   let auth_token = Sys.getenv_exn "AUTH_TOKEN" in
-  let uri = "https://boundvariable.space/communicate" in
-  let body = Lwt_main.run (get_body auth_token uri) in
+  let body = Lwt_main.run (get_body auth_token input) in
   print_endline body
