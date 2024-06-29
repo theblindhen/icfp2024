64fc7e3a5c4b6366355ff7ffe42230204f39776d
Kasper Svendsen <kaspersv@gmail.com>
2024-06-29 08:45:15 +0000

    Add ICFP term for packing lambdaman solutions efficiently

---
+++ src/lib/lambdaman_pack.ml
@@ -0,0 +1,23 @@
+open Language
+open Interpreter
+
+let decode_dir =
+  abs (fun dir ->
+      if_op (eq_op dir (Integer 0)) (String "L")
+        (if_op (eq_op dir (Integer 1)) (String "D")
+           (if_op (eq_op dir (Integer 2)) (String "U") (String "R"))))
+
+let rec_body =
+  abs (fun rec_f ->
+      abs (fun arg ->
+          if_op (eq_op arg (Integer 1)) (String "")
+            (let_op (mod_op arg (Integer 4)) (fun i ->
+                 let_op (div_op arg (Integer 4)) (fun r ->
+                     let_op (app decode_dir i) (fun c -> concat_op c (app rec_f r)))))))
+
+(** ICFP term for decoding integers to L/D/U/R efficiently *)
+let decode_dirs = app rec_op rec_body
+
+let%test_unit "decode_dirs" =
+  [%test_eq: term] (eval (app decode_dirs (Integer 121))) (String "DUR");
+  [%test_eq: term] (eval (app decode_dirs (Integer 313))) (String "DURL")
--- src/lib/language.ml
+++ src/lib/language.ml
@@ -135,6 +135,32 @@ let parse (input : string) : term =
   assert (List.is_empty rest);
   term
 
+(** Generic ICFP expression operators *)
+
+let vars = ref 1
+
+let abs body =
+  let var = !vars in
+  vars := !vars + 1;
+  Abstract (var, body (Var var))
+
+let app t1 t2 = Binary (Apply, t1, t2)
+
+(* let operator *)
+let let_op expr body = app (abs body) expr
+let if_op cond t e = If (cond, t, e)
+let eq_op t1 t2 = Binary (Equal, t1, t2)
+let take_op t1 t2 = Binary (Take, t1, t2)
+let drop_op t1 t2 = Binary (Drop, t1, t2)
+let mod_op t1 t2 = Binary (Mod, t1, t2)
+let div_op t1 t2 = Binary (Div, t1, t2)
+let concat_op t1 t2 = Binary (StringConcat, t1, t2)
+let sToI t = Unary (StringToInt, t)
+
+(* ICFP term recursion operator *)
+(* forall alpha. (alpha -> alpha) -> alpha *)
+let rec_op = abs (fun f -> app (abs (fun x -> app f (app x x))) (abs (fun x -> app f (app x x))))
+
 (* TESTS *)
 
 let%test_unit "decode_string" = [%test_eq: string] (decode_string "}Q/2,$_") " World!"
