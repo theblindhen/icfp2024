8cfc82bb8ba8fd48c538e0912650b188aa44d548
Kasper Svendsen <kaspersv@gmail.com>
2024-06-29 09:07:47 +0000

    Lambdaman: Add efficient packing

--- src/lib/lambdaman_pack.ml
+++ src/lib/lambdaman_pack.ml
@@ -19,6 +19,19 @@ let rec_body =
 (** ICFP term for decoding integers to L/D/U/R efficiently *)
 let decode_dirs = app rec_op rec_body
 
+let encode_dir dir =
+  match dir with
+  | 'L' -> 0
+  | 'D' -> 1
+  | 'U' -> 2
+  | 'R' -> 3
+  | _ -> failwith "Invalid direction"
+
+let rec encode_dirs dirs =
+  if String.length dirs = 0 then 1
+  else encode_dir dirs.[0] + (4 * encode_dirs (String.sub dirs 1 (String.length dirs - 1)))
+
 let%test_unit "decode_dirs" =
-  [%test_eq: term] (eval (app decode_dirs (Integer 121))) (String "DUR");
-  [%test_eq: term] (eval (app decode_dirs (Integer 313))) (String "DURL")
+  [%test_eq: term] (eval (app decode_dirs (Integer (encode_dirs "DUR")))) (String "DUR");
+  [%test_eq: term] (eval (app decode_dirs (Integer (encode_dirs "DURL")))) (String "DURL");
+  [%test_eq: term] (eval (app decode_dirs (Integer (encode_dirs "DDDDDDDD")))) (String "DDDDDDDD")
