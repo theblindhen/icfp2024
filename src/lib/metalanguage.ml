2f5d4a2dd1592b578ddf3047468a0cd27617eaeb
Kasper Svendsen <kaspersv@gmail.com>
2024-06-30 16:47:35 +0000

    Add random mover

--- src/lib/metalanguage.ml
+++ src/lib/metalanguage.ml
@@ -15,6 +15,7 @@ let app t1 t2 = Binary (Apply, t1, t2)
 let let_op expr body = app (abs body) expr
 let if_op cond t e = If (cond, t, e)
 let add_op t1 t2 = Binary (Add, t1, t2)
+let mult_op t1 t2 = Binary (Mul, t1, t2)
 let eq_op t1 t2 = Binary (Equal, t1, t2)
 let take_op t1 t2 = Binary (Take, t1, t2)
 let drop_op t1 t2 = Binary (Drop, t1, t2)
