b7da5388033281eacf1e1a0ea217eb6cec098afa
Kasper Svendsen <kaspersv@gmail.com>
2024-06-30 10:50:01 +0000

    Additional handwritten lambdaman6 solutions

--- src/bin/lambdaman_hand.ml
+++ src/bin/lambdaman_hand.ml
@@ -1,40 +1,98 @@
 open Core
 open Lib
+open Language
 open Metalanguage
 
 let wrap_prefix level term = concat_op (String ("solve lambdaman" ^ Int.to_string level ^ " ")) term
 
+let hand_solutions6 =
+  [
+    wrap_prefix 6
+      (let_op
+         (abs (fun a -> concat_op a (concat_op (concat_op a a) (concat_op a a))))
+         (fun mult -> app mult (app mult (String "RRRRRRRR"))));
+    wrap_prefix 6
+      (let_op
+         (abs (fun a -> concat_op a a))
+         (fun double ->
+           let_op (String "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR") (fun x ->
+               app double (app double x))));
+    wrap_prefix 6
+      (let_op
+         (abs (fun a -> concat_op a (concat_op a a)))
+         (fun double ->
+           drop_op
+             (Integer (Util.big 16))
+             (app double (app double (app double (String "RRRRRRRR"))))));
+    wrap_prefix 6
+      (let_op
+         (abs (fun a -> concat_op a (concat_op (concat_op a a) (concat_op a a))))
+         (fun mult -> app mult (app mult (String "RRRRRRRR"))));
+  ]
+
+let mults =
+  [
+    String "Not supported";
+    String "Not supported";
+    abs (fun a -> concat_op a a);
+    abs (fun a -> concat_op a (concat_op a a));
+    abs (fun a -> concat_op (concat_op a a) (concat_op a a));
+    abs (fun a -> concat_op a (concat_op (concat_op a a) (concat_op a a)));
+    abs (fun a -> concat_op (concat_op a a) (concat_op (concat_op a a) (concat_op a a)));
+    abs (fun a ->
+        concat_op a (concat_op (concat_op a a) (concat_op (concat_op a a) (concat_op a a))));
+  ]
+
+let generate_hand_solutions6 () =
+  let specs =
+    [
+      (200, ((2, 3), 25));
+      (200, ((2, 2), 50));
+      (200, ((5, 2), 8));
+      (200, ((5, 1), 40));
+      (203, ((7, 1), 29));
+      (207, ((3, 2), 23));
+      (208, ((2, 4), 13));
+      (216, ((3, 3), 8));
+      (224, ((2, 5), 7));
+      (243, ((3, 5), 1));
+      (243, ((3, 4), 3));
+      (245, ((7, 2), 5));
+      (250, ((5, 3), 2));
+      (256, ((2, 8), 1));
+      (256, ((2, 7), 2));
+      (256, ((2, 6), 4));
+      (343, ((7, 3), 1));
+      (512, ((2, 9), 1));
+      (625, ((5, 4), 1));
+      (729, ((3, 6), 1));
+    ]
+  in
+  let rec rep n f x = if n = 0 then x else rep (n - 1) f (app f x) in
+  let drop_if size t = if size > 200 then drop_op (Integer (Util.big (size - 200))) t else t in
+  List.map specs ~f:(fun (size, ((mult, power), lit_length)) ->
+      let mult_op = List.nth_exn mults mult in
+      let literal = String (String.make lit_length 'R') in
+      if power > 1 then
+        wrap_prefix 6 (let_op mult_op (fun mult -> drop_if size (rep power mult literal)))
+      else wrap_prefix 6 (drop_if size (rep power mult_op literal)))
+
+let validate_result6 res =
+  String.equal res
+    "solve lambdaman6 \
+     RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
+
 let hand_solutions =
-  [|
-    [||];
-    [||];
-    [||];
-    [||];
-    [||];
-    [|
-      wrap_prefix 6
-        (let_op
-           (abs (fun a -> concat_op a (concat_op (concat_op a a) (concat_op a a))))
-           (fun mult -> app mult (app mult (String "RRRRRRRR"))));
-      wrap_prefix 6
-        (let_op
-           (abs (fun a -> concat_op a a))
-           (fun double ->
-             let_op (String "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR") (fun x ->
-                 app double (app double x))));
-      wrap_prefix 6
-        (let_op
-           (abs (fun a -> concat_op a (concat_op a a)))
-           (fun double ->
-             drop_op
-               (Integer (Util.big 16))
-               (app double (app double (app double (String "RRRRRRRR"))))));
-      wrap_prefix 6
-        (let_op
-           (abs (fun a -> concat_op a (concat_op (concat_op a a) (concat_op a a))))
-           (fun mult -> app mult (app mult (String "RRRRRRRR"))));
-    |];
-  |]
+  [
+    [];
+    [];
+    [];
+    [];
+    [];
+    List.map
+      (List.append hand_solutions6 (generate_hand_solutions6 ()))
+      ~f:(fun x -> (x, validate_result6));
+  ]
 
 (* Main function *)
 let () =
@@ -43,10 +101,19 @@ let () =
   else
     let dir = (Sys.get_argv ()).(1) in
     let level = Int.of_string (Sys.get_argv ()).(2) in
-    if level - 1 < Array.length hand_solutions then
-      let sols = Array.get hand_solutions (level - 1) in
-      Array.iter sols ~f:(fun sol ->
+    if
+      level - 1 < List.length hand_solutions
+      && not (List.is_empty (List.nth_exn hand_solutions (level - 1)))
+    then
+      let sols = List.nth_exn hand_solutions (level - 1) in
+      List.iter sols ~f:(fun (sol, validator) ->
           let icfp = Language.deparse sol in
           printf "%5d: %s\n" (String.length icfp) icfp;
-          Solutions.write_solution "lambdaman" dir (Int.to_string level) icfp)
+          let res = Interpreter.eval sol in
+          match res with
+          | String s ->
+              if validator s then
+                Solutions.write_solution "lambdaman" dir (Int.to_string level) icfp
+              else printf "XXX Didn't validate: %s\n" s
+          | _ -> printf "XXX Didn't evaluate to a string literal: %s\n" (Language.deparse res))
     else printf "No handwritten solution for level %d\n" level
